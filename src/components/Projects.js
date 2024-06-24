db5595c1db1782828c06df45061c7eb144208ff7
Wesley Moraes <wesleycrz@hotmail.com>
2024-06-24 16:01:04 -0300

    pagina de projetos

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,34 +6,44 @@ const Projects = () =>{
             <div>
                 <h2>Projects Page</h2>
             </div>
-            <div className='lista-projetos'>
-            <ul>
-                <li>
-                    <a href="https://todolist-chi-fawn.vercel.app/" target='_blank' rel="noopener noreferrer">To Do List App </a>
-                </li>
-                <li>
-                    <a href="https://my-costs-two.vercel.app/" target='_blank' rel="noopener noreferrer">My Costs App </a>
-                </li>
-                <li>
-                    <a href="https://wesley-moraes.github.io/landingPage-Flash/" target='_blank' rel="noopener noreferrer">Landing Page Flash</a>
-                </li>
-                <li>
-                    <a href="https://wesley-moraes.github.io/DungeonChars/" target='_blank' rel="noopener noreferrer">Dungeon's Chars</a>
-                </li>
-                <li>
-                    <a href="https://quiz-react-three-dun.vercel.app/" target='_blank' rel="noopener noreferrer">Quiz React</a>
-                </li>
-                <li>
-                    <a href="https://resident-evil-2-landingpage.vercel.app/" target='_blank' rel="noopener noreferrer">Resident Evil 2</a>
-                </li>
-                <li>
-                    <a href="https://next-test-rn8i.vercel.app/" target='_blank' rel="noopener noreferrer">Next Test</a>
-                </li>
-                
-            </ul>
-                
+            <div className='projetos-box'>
+                <div className='nav-projeto-left'>
+                    <div className='lista-projetos'>
+                        <div>
+                            <ul>
+                                <li>
+                                    <a href="https://todolist-chi-fawn.vercel.app/" target='_blank' rel="noopener noreferrer">To Do List App </a>
+                                </li>
+                                <li>
+                                    <a href="https://my-costs-two.vercel.app/" target='_blank' rel="noopener noreferrer">My Costs App </a>
+                                </li>
+                                <li>
+                                    <a href="https://wesley-moraes.github.io/landingPage-Flash/" target='_blank' rel="noopener noreferrer">Landing Page Flash</a>
+                                </li>
+                                <li>
+                                    <a href="https://wesley-moraes.github.io/DungeonChars/" target='_blank' rel="noopener noreferrer">Dungeon's Chars</a>
+                                </li>
+                                <li>
+                                    <a href="https://quiz-react-three-dun.vercel.app/" target='_blank' rel="noopener noreferrer">Quiz React</a>
+                                </li>
+                                <li>
+                                    <a href="https://resident-evil-2-landingpage.vercel.app/" target='_blank' rel="noopener noreferrer">Resident Evil 2</a>
+                                </li>
+                                <li>
+                                    <a href="https://next-test-rn8i.vercel.app/" target='_blank' rel="noopener noreferrer">Next Test</a>
+                                </li>                    
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div className='prev-projeto-right'>
+                    <div className='box-prev'>
+
+                    </div>
+                </div>
             </div>
         </div>
+        
     );
 };
 
